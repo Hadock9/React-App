b3aede208dcba1335e40ed9a6ae701a7b122631b
Hadock9 <vasya.falyovskij@email.com>
2024-09-23 13:51:12 +0300

    Matches Block Upd

--- my-app/src/pages/Matches.jsx
+++ my-app/src/pages/Matches.jsx
@@ -2,6 +2,8 @@ import { BurgerMenu } from '../components/BurgerMenu'
 import { MySearch } from '../components/Search'
 import TeamLogo from '../img//TeamsLogo/egamersworld (3).png'
 import country from '../img/Countries/ua.png'
+import CsMiniLogo1 from '../img/GamesMiniLogo/counterstrike.png'
+import CsMiniLogo2 from '../img/GamesMiniLogo/counterstrikeminlogo.png'
 import style from '../styles/Matches.module.css'
 export function Matches() {
 	return (
@@ -10,7 +12,7 @@ export function Matches() {
 				<BurgerMenu />
 				<main className={style.Main}>
 					<MySearch />
-					<h1>Matches 1</h1>
+					<h1>Matches</h1>
 					<div className={style.MatchesBlock}>
 						<div className={style.MatchesBlockInfo}>
 							<div className={style.MatchesBlockTeam}>
@@ -19,18 +21,23 @@ export function Matches() {
 										className={style.MatchesBlockImgCountry}
 										src={country}
 										alt=''
-									/>{' '}
+									/>
 								</div>
 								<img
 									className={style.MatchesBlockImgLogo}
 									src={TeamLogo}
 									alt=''
 								/>
+								<div
+									className={`${style.MatchesBlockCoef} ${style.MatchesBlockTeam1Coef}`}
+								>
+									<p>1.43</p>
+								</div>
 							</div>
-							<div className={style.MatchesBlockVs}>
-								<h3></h3>
-								<p> Vs</p>
-								<p>Bo3</p>
+							<div className={style.MatchesBlockCenter}>
+								<p className={style.MatchesBlockVsDateTime}>23.09.24 12:00</p>
+								<p className={style.MatchesBlockVs}>Vs</p>
+								<p className={style.MatchesBlockPlace}> European</p>
 							</div>
 							<div className={style.MatchesBlockTeam2}>
 								<div className={style.MatchesBlockCountry}>
@@ -44,6 +51,30 @@ export function Matches() {
 								<img
 									className={style.MatchesBlockImgLogo}
 									src={TeamLogo}
+									alt=''
+								/>
+								<div
+									className={`${style.MatchesBlockCoef} ${style.MatchesBlockTeam2Coef}`}
+								>
+									<p>1.43</p>
+								</div>
+							</div>
+						</div>
+						<div className={style.MatchesBlockSide}>
+							<div className={style.MatchesSideBlockImg1}>
+								<img
+									className={style.MatchesSideImg}
+									src={CsMiniLogo1}
+									alt=''
+								/>
+							</div>
+							<div className={style.MatchesSideText}>
+								<p>European Pro League Season 19</p>
+							</div>
+							<div className={style.MatchesSideBlockImg}>
+								<img
+									className={style.MatchesSideImg}
+									src={CsMiniLogo2}
 									alt=''
 								/>
 							</div>
