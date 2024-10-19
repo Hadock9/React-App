42237afc715396e150ed89caa6e69fd403c55546
voron <voronych.nestor@gmail.com>
2024-10-19 07:17:16 -0400

    minor rounding changes

--- frontend/src/components/BlockSaveUkraine.jsx
+++ frontend/src/components/BlockSaveUkraine.jsx
@@ -1,15 +1,13 @@
 import { Link } from 'react-router-dom'
 export function UkrainianWar() {
-	return (
-		<Link to='https://savelife.in.ua/en/'>
-			<div className='fixed right-[20px] bottom-[70px] z-50 w-[120px] h-[70px] text-center '>
-				<div className='bg-[#0292c4] h-1/2 w-full flex justify-center items-center'>
-					#UkrainianWar
-				</div>
-				<div className='bg-[#f5c142] h-1/2 w-full flex justify-center items-center'>
-					Support
-				</div>
-			</div>
-		</Link>
-	)
+    return (
+        <Link to="https://savelife.in.ua/en/">
+            <div className="fixed right-[20px] bottom-[70px] z-50 w-[120px] h-[70px] text-center">
+                <div className="bg-[#0292c4] h-1/2 w-full flex justify-center items-center rounded-t-lg">
+                    #UkrainianWar
+                </div>
+                <div className="bg-[#f5c142] h-1/2 w-full flex justify-center items-center rounded-b-lg">Support</div>
+            </div>
+        </Link>
+    )
 }
