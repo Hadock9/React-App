ddffb4f08ff42aa646b38d5fff4e3e11b7373466
Hadock <vasya.falyovskij@gmail.com>
2024-10-23 16:27:48 +0300

    upd burger menu comments navbar logo

--- frontend/src/components/BurgerMenu.jsx
+++ frontend/src/components/BurgerMenu.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import {
 	CircleDollarSign,
 	CircleUserRound,
@@ -8,9 +9,8 @@ import {
 	Menu,
 	Wallet,
 } from 'lucide-react'
-import { useMenu } from '../context/MenuContext'
-
 import { Link } from 'react-router-dom'
+import { useMenu } from '../context/MenuContext'
 import style from '../styles/BurgerMenu.module.css'
 
 export function BurgerMenu() {
@@ -20,10 +20,35 @@ export function BurgerMenu() {
 		setIsOpen(prev => !prev)
 	}
 
+	const textAnimations = {
+		hidden: { opacity: 0, x: -20 },
+		show: { opacity: 1, x: 0 },
+		exit: { opacity: 0, x: -20 },
+	}
+
+	const transition = {
+		duration: 0.6,
+		ease: 'easeInOut',
+	}
+
+	const AnimatedText = ({ children }) => (
+		<AnimatePresence>
+			{isOpen && (
+				<motion.div
+					initial='hidden'
+					animate='show'
+					exit='exit'
+					variants={textAnimations}
+					transition={transition}
+				>
+					<p>{children}</p>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	)
+
 	return (
-		<aside
-			className={isOpen ? style.AsideContainer : style.AsideContainerHidden}
-		>
+		<motion.aside animate={{ width: isOpen ? '350px' : '60px' }}>
 			<div className={style.AsideBlockBrowse} onClick={toggleMenu}>
 				<div className={style.AsideBlockIconBrowse}>
 					<Menu />
@@ -35,59 +60,45 @@ export function BurgerMenu() {
 					<div className={style.AsideBlockIcon}>
 						<House />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Головна</p>
-					</div>
+					<AnimatedText>Головна</AnimatedText>
 				</Link>
 				<Link to='/Stake' className={style.AsideBlock}>
 					<div className={style.AsideBlockIcon}>
 						<History />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Історія ставок</p>
-					</div>
+					<AnimatedText>Історія ставок</AnimatedText>
 				</Link>
 				<Link to='/Balance' className={style.AsideBlock}>
 					<div className={style.AsideBlockIcon}>
 						<CircleDollarSign />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Управління Балансом</p>
-					</div>
+					<AnimatedText>Баланс</AnimatedText>
 				</Link>
 				<Link to='/Home' className={style.AsideBlock}>
 					<div className={style.AsideBlockIcon}>
 						<Gem />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Бонуси</p>
-					</div>
+					<AnimatedText>Бонуси</AnimatedText>
 				</Link>
 				<Link to='/Profile' className={style.AsideBlock}>
 					<div className={style.AsideBlockIcon}>
 						<CircleUserRound />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Мій профіль</p>
-					</div>
+					<AnimatedText>Мій профіль</AnimatedText>
 				</Link>
 				<Link to='/Home' className={style.AsideBlock}>
 					<div className={style.AsideBlockIcon}>
 						<Wallet />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Гаманець</p>
-					</div>
+					<AnimatedText>Гаманець</AnimatedText>
 				</Link>
 				<Link to='/Notifications' className={style.AsideBlock}>
 					<div className={style.AsideBlockIcon}>
 						<Mail />
 					</div>
-					<div className={isOpen ? style.AsideBlockText : style.HiddenBlock}>
-						<p>Повідомлення</p>
-					</div>
+					<AnimatedText>Повідомлення</AnimatedText>
 				</Link>
 			</>
-		</aside>
+		</motion.aside>
 	)
 }
--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -111,7 +111,7 @@ const Comments = ({ id }) => {
 							onBlur={SetCommentTextDirty}
 							value={CommentText}
 							onChange={handleCommentText}
-							className='resize-y min-h-[80px] max-h-[300px] overflow-auto w-[90%] h-[100px] p-2 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f5ba1a] focus:border-[#f5ba1a] text-gray-700 placeholder-gray-500'
+							className='resize-y min-h-[80px] max-h-[300px] overflow-auto w-[90%] h-[100px] p-2 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f5ba1a] focus:border-[#f5ba1a] text-gray-700 placeholder-gray-500 '
 							placeholder='Введіть свій коментар...'
 						></motion.textarea>
 
