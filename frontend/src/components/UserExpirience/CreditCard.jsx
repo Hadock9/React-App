b40269749ee3a3f8f82f180eaecd25e15c7ad035
voron <voronych.nestor@gmail.com>
2024-10-31 00:34:20 -0400

    card validation

--- frontend/src/components/UserExpirience/CreditCard.jsx
+++ frontend/src/components/UserExpirience/CreditCard.jsx
@@ -1,15 +1,84 @@
 import Mybutton from '../../UI/Mybutton'
+import { useEffect, useState } from 'react'
+import TextError from '../../UI/TextError'
+
+import {
+    validateCardName,
+    validateCreditCard,
+    validateExpDate,
+    validateCVV,
+} from '../../js/FormValidation.js'
 
 export function CreditCard() {
+    const [Card, setCard] = useState('')
+    const [Name, setName] = useState('')
+    const [ExpDate, setExpDate] = useState('')
+    const [CVV, setCVV] = useState('')
+
+    const [CardDirty, setCardDirty] = useState(false)
+    const [NameDirty, setNameDirty] = useState(false)
+    const [ExpDateDirty, setExpDateDirty] = useState(false)
+    const [CVVDirty, setCVVDirty] = useState(false)
+
+    const [CardError, setCardError] = useState(
+        'Номер карти не може бути пустим'
+    )
+    const [NameError, setNameError] = useState(
+        "Ім'я власника не може бути пустим"
+    )
+    const [ExpDateError, setExpDateError] = useState(
+        'Поле з датою не може бути пустим'
+    )
+    const [CVVError, setCVVError] = useState('CVV номер не може бути пустим')
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'Card':
+                setCardDirty(true)
+                break
+            case 'Name':
+                setNameDirty(true)
+                break
+            case 'ExpDate':
+                setExpDateDirty(true)
+                break
+            case 'CVV':
+                setCVVDirty(true)
+                break
+            default:
+                break
+        }
+    }
+
+    const CardHandler = (e) => {
+        setCard(e.target.value)
+        setCardError(validateCreditCard(e.target.value))
+    }
+    const NameHandler = (e) => {
+        setName(e.target.value)
+        setNameError(validateCardName(e.target.value))
+    }
+    const ExpDateHandler = (e) => {
+        setExpDate(e.target.value)
+        setExpDateError(validateExpDate(e.target.value))
+    }
+    const CVVHandler = (e) => {
+        setCVV(e.target.value)
+        setCVVError(validateCVV(e.target.value))
+    }
     return (
         <div className=" ">
-            <div className="absolute -z-10"></div>
+            {/*  */}
+            <TextError TextDirty={CardDirty} TextError={CardError} />
+            <TextError TextDirty={NameDirty} TextError={NameError} />
+            <TextError TextDirty={ExpDateDirty} TextError={ExpDateError} />
+            <TextError TextDirty={CVVDirty} TextError={CVVError} />
 
+            <div className="absolute -z-10"></div>
             {/* left-[250px] */}
             <div className="absolute rounded-full bg-gradient-to-br from-red-500 bg-red-950 opacity-80 h-[240px] w-[240px] left-[55%] top-[10%]"></div>
             {/* top-[120px] left-[-50px] */}
             <div className="absolute rounded-full bg-gradient-to-br from-red-500 bg-red-950 opacity-80 h-[300px] w-[300px] left-[35%] top-[20%]"></div>
-
             <div className=" flex flex-col items-center">
                 <div className=" flex flex-col h-[280px] w-[420px] rounded-2xl bg-black bg-opacity-10 backdrop-blur-lg border border-black border-opacity-10 shadow-lg p-5">
                     <div className="flex justify-end">
@@ -31,7 +100,11 @@ export function CreditCard() {
                     <input
                         className="w-full h-12 text-white placeholder-white text-xl bg-transparent placeholder-gray-400 border-b border-white focus:outline-none mb-8"
                         placeholder="1234 1234 1234 1234"
-                        type="text"
+                        type="telephone"
+                        name="Card"
+                        value={Card}
+                        onBlur={blurHandler}
+                        onChange={CardHandler}
                         maxLength="16"
                         required
                     />
@@ -45,6 +118,10 @@ export function CreditCard() {
                                 className="text-white placeholder-white bg-transparent placeholder-gray-400 border-b border-white focus:outline-none"
                                 placeholder="JOHN DOE"
                                 type="text"
+                                name="Name"
+                                value={Name}
+                                onBlur={blurHandler}
+                                onChange={NameHandler}
                                 required
                             />
                         </div>
@@ -57,6 +134,10 @@ export function CreditCard() {
                                 className="text-white placeholder-white bg-transparent placeholder-gray-400 border-b border-white focus:outline-none"
                                 placeholder="10/25"
                                 type="text"
+                                name="ExpDate"
+                                value={ExpDate}
+                                onBlur={blurHandler}
+                                onChange={ExpDateHandler}
                                 maxLength="5"
                                 required
                             />
@@ -71,6 +152,10 @@ export function CreditCard() {
                                 placeholder="123"
                                 type="text"
                                 maxLength="3"
+                                name="CVV"
+                                value={CVV}
+                                onBlur={blurHandler}
+                                onChange={CVVHandler}
                                 required
                             />
                         </div>
--- frontend/src/js/FormValidation.js
+++ frontend/src/js/FormValidation.js
@@ -1,95 +1,141 @@
 // Валідація Email
-export const validateEmail = email => {
-	const re =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i
-	return re.test(String(email).toLowerCase()) ? '' : 'Email не правильний'
+export const validateEmail = (email) => {
+    const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i
+    return re.test(String(email).toLowerCase()) ? '' : 'Email не правильний'
 }
 
 // Валідація пароля
-export const validatePassword = password => {
-	const re =
-		/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,16}$/
+export const validatePassword = (password) => {
+    const re =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,16}$/
 
-	if (!password) {
-		return 'Поле не може бути пустим'
-	} else if (password.length < 6 || password.length > 16) {
-		return 'Поле має бути від 6 до 16 символів'
-	} else if (!re.test(String(password))) {
-		return 'Пароль повинен містити хоча б одну цифру,одну велику літеру,одну малу літеру і один спеціальний символ (!@#$%^&*)'
-	}
-	return ''
+    if (!password) {
+        return 'Поле не може бути пустим'
+    } else if (password.length < 6 || password.length > 16) {
+        return 'Поле має бути від 6 до 16 символів'
+    } else if (!re.test(String(password))) {
+        return 'Пароль повинен містити хоча б одну цифру,одну велику літеру,одну малу літеру і один спеціальний символ (!@#$%^&*)'
+    }
+    return ''
 }
 
 // Валідація повторного пароля
 export const validateRePassword = (rePassword, password) => {
-	if (!rePassword) {
-		return 'Поле не може бути пустим'
-	} else if (rePassword !== password) {
-		return 'Паролі не збігаються'
-	}
-	return ''
+    if (!rePassword) {
+        return 'Поле не може бути пустим'
+    } else if (rePassword !== password) {
+        return 'Паролі не збігаються'
+    }
+    return ''
 }
 
 // Валідація імені
-export const validateName = name => {
-	const re = /^[A-ZА-ЯІЇЄ][a-zа-яіїє'їє]+(?:[- ][A-ZА-ЯІЇЄ][a-zа-яїє'їє]+)?$/
-	return re.test(name)
-		? ''
-		: "Ім'я повинно містити тільки літери, перша літера мaє бути великою, а решта — маленькими, і можливий один пробіл між словами (якщо ім'я подвійне)."
+export const validateName = (name) => {
+    const re = /^[A-ZА-ЯІЇЄ][a-zа-яіїє'їє]+(?:[- ][A-ZА-ЯІЇЄ][a-zа-яїє'їє]+)?$/
+    return re.test(name)
+        ? ''
+        : "Ім'я повинно містити тільки літери, перша літера мaє бути великою, а решта — маленькими, і можливий один пробіл між словами (якщо ім'я подвійне)."
 }
 // Валідація прізвища
-export const validateLastName = name => {
-	const re = /^[A-ZА-ЯІЇЄ][a-zа-яіїє'їє]+(?:[- ][A-ZА-ЯІЇЄ][a-zа-яїє'їє]+)?$/
-	return re.test(name)
-		? ''
-		: 'Прізвище повинно містити тільки літери, перша літера мaє бути великою, а решта — маленькими, і можливий один пробіл між словами (якщо прізвище подвійне).'
+export const validateLastName = (name) => {
+    const re = /^[A-ZА-ЯІЇЄ][a-zа-яіїє'їє]+(?:[- ][A-ZА-ЯІЇЄ][a-zа-яїє'їє]+)?$/
+    return re.test(name)
+        ? ''
+        : 'Прізвище повинно містити тільки літери, перша літера мaє бути великою, а решта — маленькими, і можливий один пробіл між словами (якщо прізвище подвійне).'
 }
 
 // Валідація умов
-export const validateConditions = conditions => {
-	return conditions ? '' : 'Ви повинні погодитися з умовами.'
+export const validateConditions = (conditions) => {
+    return conditions ? '' : 'Ви повинні погодитися з умовами.'
 }
 
 // Валідація дати народження
-export const validateDate_of_birth = Date_of_birth => {
-	// Перетворюємо рядок дати на об'єкт Date
-	const dateOfBirth = new Date(Date_of_birth)
-	const date_now = new Date()
+export const validateDate_of_birth = (Date_of_birth) => {
+    // Перетворюємо рядок дати на об'єкт Date
+    const dateOfBirth = new Date(Date_of_birth)
+    const date_now = new Date()
 
-	// Мінімальна дата (1 січня 1900 року)
-	const date_min = new Date('1900-01-01')
+    // Мінімальна дата (1 січня 1900 року)
+    const date_min = new Date('1900-01-01')
 
-	// Перевірка
-	if (dateOfBirth > date_min && dateOfBirth < date_now) {
-		return '' // Дата дійсна
-	} else {
-		return 'Дата повинна бути більшою ніж 1900 рік, але меншою ніж сьогодення'
-	}
+    // Перевірка
+    if (dateOfBirth > date_min && dateOfBirth < date_now) {
+        return '' // Дата дійсна
+    } else {
+        return 'Дата повинна бути більшою ніж 1900 рік, але меншою ніж сьогодення'
+    }
 }
 
-export const validatePhone = Phone => {
-	const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-	return re.test(Phone) ? '' : 'Телефон введено не правильно...'
+export const validatePhone = (Phone) => {
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    return re.test(Phone) ? '' : 'Телефон введено не правильно...'
 }
 
-export const validateTextArea = textArea => {
-	const trimmedText = textArea.trim()
+export const validateTextArea = (textArea) => {
+    const trimmedText = textArea.trim()
 
-	if (!trimmedText) {
-		return 'Поле не може бути пустим'
-	} else if (trimmedText.length < 4 || trimmedText.length > 500) {
-		return `Поле має бути від 4 до 500 символів. Кількість символів: ${trimmedText.length}`
-	}
+    if (!trimmedText) {
+        return 'Поле не може бути пустим'
+    } else if (trimmedText.length < 4 || trimmedText.length > 500) {
+        return `Поле має бути від 4 до 500 символів. Кількість символів: ${trimmedText.length}`
+    }
 
-	return ''
+    return ''
 }
 
 export const validateStakeAmount = (Amount, UserBalance) => {
-	if (Amount == 0) {
-		return 'Поле не може  != 0'
-	} else if (Amount < 0 || Amount > UserBalance) {
-		return `Поле має бути більшим за 0  та меншим за ваш баланс ${UserBalance}`
-	}
+    if (Amount == 0) {
+        return 'Поле не може  != 0'
+    } else if (Amount < 0 || Amount > UserBalance) {
+        return `Поле має бути більшим за 0  та меншим за ваш баланс ${UserBalance}`
+    }
 
-	return ''
+    return ''
+}
+
+// Валідація імені на картці
+export const validateCardName = (Name) => {
+    if (!Name) return "Ім'я власника не може бути пустим"
+    const re = /^[A-Z][a-z]+( [A-Z][a-z]+)*$/
+    return re.test(Name)
+        ? ''
+        : "Ім'я повинно містити два слова, перша літера кожного слова мaє бути великою."
+}
+
+export const validateCreditCard = (Card) => {
+    if (!Card) return 'Номер карти не може бути пустим'
+    const visaMasterPattern = /^(4\d{15}|5[1-5]\d{14}|2[2-7]\d{14})$/
+
+    if (!visaMasterPattern.test(Card))
+        return 'Дані ведені неправильно або дана карта не є дійсною'
+}
+
+export const validateExpDate = (expDate) => {
+    if (!expDate) return 'Поле з датою не може бути пустим'
+
+    const expPattern = /^(0[1-9]|1[0-2])\/\d{2}$/
+    if (!expPattern.test(expDate)) {
+        return 'Неправильний формат дати'
+    }
+
+    const [month, year] = expDate.split('/').map(Number)
+
+    const currentDate = new Date()
+    const currentMonth = currentDate.getMonth() + 1
+    const currentYear = currentDate.getFullYear() % 100
+
+    if (year < currentYear || (year === currentYear && month < currentMonth)) {
+        return 'Термін карти збіг кінця'
+    }
+
+    return ''
+}
+
+export const validateCVV = (cvv) => {
+    if (!cvv) return 'Поле з кодом безпеки не може бути пустим'
+
+    const cvvPattern = /^\d{3}$/
+    if (!cvvPattern.test(cvv))
+        return 'Поле з кодом безпеки заповнено неправильно'
 }
