7c9abc8dada74765ebe0e3c3a4e2ae3a5ff76a85
voron <voronych.nestor@gmail.com>
2024-11-14 01:24:09 -0500

    profile upd

--- frontend/src/components/Match/StakeForm.jsx
+++ frontend/src/components/Match/StakeForm.jsx
@@ -27,77 +27,73 @@ export default function StakeForm({ MatchGet, amount, handleSubmit, setAmount, u
     return (
         <div className='flex flex-col w-full'>
             <Toaster position='top-center' reverseOrder={false} />
-            <div className='w-full'>
-                {Match ? (
-                    <div className='flex items-center bg-gray-700 rounded-lg  p-4 justify-around'>
-                        <div class='relative'>
-                            <img
-                                src={Match.TeamCountry}
-                                alt='Larger Image'
-                                class='h-32 w-52     rounded-lg'
-                            />
-                            <div class='absolute inset-0 bg-gradient-to-br from-transparent to-gray-700 to-80% opacity-70 rounded-lg'></div>
 
-                            <img
-                                src={Match.TeamLogo}
-                                alt='Smaller Image'
-                                class='absolute inset-0 ml-auto mr-[5%] w-24 h-24'
+            {Match ? (
+                <div className='flex items-center bg-gray-700 rounded-lg  p-4 justify-around'>
+                    <div class='relative'>
+                        <img
+                            src={Match.TeamCountry}
+                            alt='Larger Image'
+                            class='h-32 w-52     rounded-lg'
+                        />
+                        <div class='absolute inset-0 bg-gradient-to-br from-transparent to-gray-700 to-80% opacity-70 rounded-lg'></div>
+
+                        <img
+                            src={Match.TeamLogo}
+                            alt='Smaller Image'
+                            class='absolute inset-0 ml-auto mr-[5%] w-24 h-24'
+                        />
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <label className='text-white text-s font-light mb-1'>
+                            Ставка на команду
+                        </label>
+                        <div className='text-white text-lg'>{Match.TeamName}</div>
+                    </div>
+
+                    <form onSubmit={handleSubmit} className='flex gap-3'>
+                        <div className='flex flex-col items-center'>
+                            <label className='text-white text-s font-light mb-1'>Сума ставки</label>
+                            <input
+                                className='text-end w-32 text-white text-3xl bg-transparent placeholder-gray-400 border-b border-white focus:outline-none'
+                                placeholder='0'
+                                maxLength={6}
+                                name='amount'
+                                onBlur={() => setAmountDirty(true)}
+                                value={amount}
+                                onChange={handleAmount}
+                                required
                             />
+                        </div>
+                        <div className='flex items-end pb-1 text-white'>
+                            <X />
                         </div>
                         <div className='flex flex-col items-center'>
-                            <label className='text-white text-s font-light mb-1'>
-                                Ставка на команду
-                            </label>
-                            <div className='text-white text-lg'>{Match.TeamName}</div>
+                            <label className='text-white text-s font-light mb-1'>Коефіцієнт</label>
+                            <div className='text-white text-3xl'>{Match.TeamCoef}</div>
+                        </div>
+                        <div className='flex text-white items-end pb-1'>
+                            <Equal />
                         </div>
 
-                        <form onSubmit={handleSubmit} className='flex gap-3'>
-                            <div className='flex flex-col items-center'>
-                                <label className='text-white text-s font-light mb-1'>
-                                    Сума ставки
-                                </label>
-                                <input
-                                    className='text-end w-32 text-white text-3xl bg-transparent placeholder-gray-400 border-b border-white focus:outline-none'
-                                    placeholder='0'
-                                    maxLength={6}
-                                    name='amount'
-                                    onBlur={() => setAmountDirty(true)}
-                                    value={amount}
-                                    onChange={handleAmount}
-                                    required
-                                />
+                        <div className='flex flex-col items-center'>
+                            <label className='text-white text-s font-light mb-1'>
+                                Потенційний виграш
+                            </label>
+                            <div className='text-white text-3xl'>
+                                {(amount * Match.TeamCoef).toFixed(2)}₴
                             </div>
-                            <div className='flex items-end pb-1 text-white'>
-                                <X />
-                            </div>
-                            <div className='flex flex-col items-center'>
-                                <label className='text-white text-s font-light mb-1'>
-                                    Коефіцієнт
-                                </label>
-                                <div className='text-white text-3xl'>{Match.TeamCoef}</div>
-                            </div>
-                            <div className='flex text-white items-end pb-1'>
-                                <Equal />
-                            </div>
+                        </div>
+                        <div className='flex justify-center w-52'>
+                            <Mybutton ondisable={!isDisabled}>Submit</Mybutton>
+                        </div>
+                    </form>
+                </div>
+            ) : (
+                <div>Завантаження даних про матч...</div>
+            )}
 
-                            <div className='flex flex-col items-center'>
-                                <label className='text-white text-s font-light mb-1'>
-                                    Потенційний виграш
-                                </label>
-                                <div className='text-white text-3xl'>
-                                    {(amount * Match.TeamCoef).toFixed(2)}₴
-                                </div>
-                            </div>
-                            <div className='flex justify-center w-52'>
-                                <Mybutton ondisable={!isDisabled}>Submit</Mybutton>
-                            </div>
-                        </form>
-                    </div>
-                ) : (
-                    <div>Завантаження даних про матч...</div>
-                )}
-                <TextError className='w-full' TextDirty={amountDirty} TextError={amountError} />
-            </div>
+            <TextError TextDirty={amountDirty} TextError={amountError} />
         </div>
     )
 }
