f427e3dcd42badffbb00d46d12544642ad47f8a5
Василь <117766007+Hadock9@users.noreply.github.com>
2024-11-16 17:09:59 +0200

    Create webpack.config.js

---
+++ frontend/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+  // 1. Точка входу
+  entry: './src/index.js',
+
+  // 2. Вихідний файл
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  // 3. Режим
+  mode: 'development', // Змініть на 'production' для продакшн
+
+  // 4. Завантажувачі
+  module: {
+    rules: [
+      // JavaScript (Babel)
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // CSS
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Зображення
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  // 5. Плагіни
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  // 6. DevServer (опціонально)
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    allowedHosts: 'all',
+  },
+};
