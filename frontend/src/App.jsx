26e1c03a25e7a5ccfb70db846e95bd975b4c4321
voron <voronych.nestor@gmail.com>
2024-11-29 15:03:25 -0500

    profile and notification upd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,59 +20,59 @@ import { Erorpage } from './pages/User/404'
 import Balance from './pages/User/Balance'
 import Home from './pages/User/Home'
 import Notifications from './pages/User/Notifications'
-import { Profile } from './pages/User/ProfileLegacy'
+import { Profile } from './pages/User/Profile'
 import Support from './pages/User/Support'
 import ProtectedRoute from './ProtectedRoute'
 
 const App = () => {
-	return (
-		<>
-			<Toaster />
-			<AnimatePresence mode='wait'>
-				<Routes>
-					<Route index element={<Navigate to='/Home' />} />
-					<Route path='/Registration' element={<Registration />} />
-					<Route path='/Login' element={<Login />} />
-					<Route path='/Games' element={<Games />} />
-					<Route path='/Home' element={<Home />} />
-					<Route path='/News' element={<News />} />
-					<Route path='/News/:Content' element={<NewsContent />} />
-					<Route path='/Stake' element={<Stake />} />
-					<Route path='/Profile' element={<Profile />} />
-					<Route path={'Games/:Game/Matches'} element={<Matches />} />
-					<Route path={'/Matches'} element={<Matches />} />
-					<Route path={'/Matches/:Match'} element={<Match />} />
-					<Route path={'Games/:Game/Matches/:Match'} element={<Match />} />
-					<Route path='/404' element={<Erorpage />} />
-					<Route path='/Notifications' element={<Notifications />} />
-					<Route path='/Balance' element={<Balance />} />
-					<Route path='/Donate' element={<Donate />} />
-					<Route path='/ResetPassword' element={<ResetPassword />} />
-					<Route path='/Feedback' element={<Support />} />
-					<Route path='/FAQ' element={<FAQ />} />
-					<Route path='*' element={<Erorpage />} />
+    return (
+        <>
+            <Toaster />
+            <AnimatePresence mode='wait'>
+                <Routes>
+                    <Route index element={<Navigate to='/Home' />} />
+                    <Route path='/Registration' element={<Registration />} />
+                    <Route path='/Login' element={<Login />} />
+                    <Route path='/Games' element={<Games />} />
+                    <Route path='/Home' element={<Home />} />
+                    <Route path='/News' element={<News />} />
+                    <Route path='/News/:Content' element={<NewsContent />} />
+                    <Route path='/Stake' element={<Stake />} />
+                    <Route path='/Profile' element={<Profile />} />
+                    <Route path={'Games/:Game/Matches'} element={<Matches />} />
+                    <Route path={'/Matches'} element={<Matches />} />
+                    <Route path={'/Matches/:Match'} element={<Match />} />
+                    <Route path={'Games/:Game/Matches/:Match'} element={<Match />} />
+                    <Route path='/404' element={<Erorpage />} />
+                    <Route path='/Notifications' element={<Notifications />} />
+                    <Route path='/Balance' element={<Balance />} />
+                    <Route path='/Donate' element={<Donate />} />
+                    <Route path='/ResetPassword' element={<ResetPassword />} />
+                    <Route path='/Feedback' element={<Support />} />
+                    <Route path='/FAQ' element={<FAQ />} />
+                    <Route path='*' element={<Erorpage />} />
 
-					{/* Захищені адмінські маршрути */}
-					<Route
-						path='/admin/*'
-						element={
-							<ProtectedRoute requiredRole='admin'>
-								<AdminDashboard />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path='support/*'
-						element={
-							<ProtectedRoute requiredRole='support'>
-								<SupportDashboard />
-							</ProtectedRoute>
-						}
-					/>
-				</Routes>
-			</AnimatePresence>
-		</>
-	)
+                    {/* Захищені адмінські маршрути */}
+                    <Route
+                        path='/admin/*'
+                        element={
+                            <ProtectedRoute requiredRole='admin'>
+                                <AdminDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path='support/*'
+                        element={
+                            <ProtectedRoute requiredRole='support'>
+                                <SupportDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Routes>
+            </AnimatePresence>
+        </>
+    )
 }
 
 export default App
