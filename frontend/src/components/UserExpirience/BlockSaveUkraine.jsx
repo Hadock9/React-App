2623be4c0bcb0856557bd2be1587255de5bc3f27
unknown <va.dmytryk@gmail.com>
2024-11-29 22:09:49 +0200

    fsfgs

--- frontend/src/components/UserExpirience/BlockSaveUkraine.jsx
+++ frontend/src/components/UserExpirience/BlockSaveUkraine.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 export function UkrainianWar() {
     return (
         <Link to="https://savelife.in.ua/en/">
-            <div className="fixed right-[50px] bottom-[50px] z-50 w-[120px] h-[70px] text-center">
+            <div className="fixed right-[50px] bottom-[50px] z-50 w-[120px] h-[70px] text-center hidden lg:block">
                 <div className="bg-[#0292c4] h-1/2 w-full flex justify-center items-center">
                     #UkrainianWar
                 </div>
